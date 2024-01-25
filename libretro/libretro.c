ddc5688085b68d5ab3a9a46d8b5f5dd486023068
libretroadmin <reallibretroretroarch@gmail.com>
2024-01-25 18:20:56 +0100

    Updates

--- libretro/libretro.c
+++ libretro/libretro.c
@@ -359,7 +359,7 @@ void FCEUD_DispMessage(enum retro_log_level level, unsigned duration, const char
 
 void FCEUD_SoundToggle (void)
 {
-   FCEUI_SetSoundVolume(sndvolume);
+   FSettings.SoundVolume = sndvolume;
 }
 
 /*palette for FCEU*/
@@ -1825,7 +1825,8 @@ static void FCEUD_RegionOverride(unsigned region)
 void retro_deinit (void)
 {
    FCEUI_CloseGame();
-   FCEUI_Sound(0);
+   FSettings.SndRate     = 0;
+   SetSoundVariables();
    FCEUI_Kill();
 #if defined(_3DS)
    linearFree(fceu_video_out);
@@ -2245,7 +2246,10 @@ static void check_variables(bool startup)
       else if (!strcmp(var.value, "Very High"))
          sndquality = 2;
       if (sndquality != oldval)
-         FCEUI_SetSoundQuality(sndquality);
+      {
+	      FSettings.soundq = sndquality;
+	      SetSoundVariables();
+      }
    }
 
    var.key = "fceumm_sndlowpass";
@@ -2253,7 +2257,7 @@ static void check_variables(bool startup)
    if (environ_cb(RETRO_ENVIRONMENT_GET_VARIABLE, &var) && var.value)
    {
       int lowpass = (!strcmp(var.value, "enabled")) ? 1 : 0;
-      FCEUI_SetLowPass(lowpass);
+      FSettings.lowpass = lowpass;
    }
 
    var.key = "fceumm_sndstereodelay";
@@ -3523,8 +3527,9 @@ bool retro_load_game(const struct retro_game_info *info)
 
    FCEUI_Initialize();
 
-   FCEUI_SetSoundVolume(sndvolume);
-   FCEUI_Sound(sndsamplerate);
+   FSettings.SoundVolume = sndvolume;
+   FSettings.SndRate     = sndsamplerate;
+   SetSoundVariables();
 
    GameInfo = (FCEUGI*)FCEUI_LoadGame(content_path, content_data, content_size,
          frontend_post_load_init);
